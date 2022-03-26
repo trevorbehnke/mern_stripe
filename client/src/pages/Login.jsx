9354f3db727b95a4afad4bde62d7a5554ae21069
tab <trevorbehnke@gmail.com>
2022-03-26 14:31:57 -0700

    page components and routing

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+function Login() {
+  return (
+    <div className="d-flex justify-content-center" style={{ height: "80vh" }}>
+      <div className="container align-items-center d-flex">
+        <div className="row col-md-6 offset-md-3 text-center">
+          <h1 className="pt-5 fw-bold">Login</h1>
+          <p className="lead pb-4">...</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,16 @@
+function Register() {
+  return (
+    <div className="d-flex justify-content-center" style={{ height: "80vh" }}>
+      <div className="container align-items-center d-flex">
+        <div className="row col-md-6 offset-md-3 text-center">
+          <h1 className="pt-5 fw-bold">Let's Get Started</h1>
+          <p className="lead pb-4">
+            Sign up for free. No credit card required.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
